f457db119639625d95843d10ae9ab45bb9bc539c
Saahil Afaq <38484758+Saahil97@users.noreply.github.com>
2018-08-05 10:56:00 +0530

    Add files via upload

---
+++ Chinese/WTChinese.php
@@ -0,0 +1,77 @@
+﻿<!DOCTYPE html>
+
+
+<html id="current">
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>PRESIDENCY CANTEEN</title>
+	<link rel="icon" href="http://localhost/WTProject/Home/title.png" type="image/x-icon">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<p id="back_top"></p>
+  <div class="main">
+  <form action="../Orders/Orders.php" method="post">
+  <form action="../Orders/Orders2.php" method="post">
+  
+    <table class="t" border="0px">
+	<tr margin="2px" ></tr>
+		<td rowspan="3" width="20%" ><img src="chicken-manchu.jpg" alt="Manchurian" width="125px" height="120px" ></td>
+		<td><b style="font-family : Arial, Helvetica, sans-serif;">Munchurian</b></td>
+		<td><center><img src="http://localhost/WTProject/Home/VegSymb.png" width="25px" height="25px"></center></td>
+	</tr>
+			<td width="80%">
+				
+			</td>
+			<td rowspan="2"><center><label class="container">
+											<input type="checkbox" name="Munchurian" value="Munchurian 80"/>
+											<span class="checkmark"></span></label></center></td>
+		</tr>	
+		<tr>
+		
+			<td><pre><h3><b>Rs 80 </b></h3></pre></td>
+		</tr>
+	</tr>
+	</table>
+
+
+<table class="t" border="0px" >
+	<tr margin="2px" ></tr>
+	<tr>
+		<td rowspan="2" width="20%" ><img src="Momo.jpg" width="125px" height="120px" ></td>
+		<td width="80%"><b style="font-family : Arial, Helvetica, sans-serif;">Momos(Dim Sums)</b></td>
+		<td><center><img src="http://localhost/WTProject/Home/VegSymb.png" width="25px" height="25px"></center></td>
+	</tr>
+	<tr>
+		<td><pre><h3><b>Rs 60 </b></h3></pre></td>
+		<td rowspan="2"><center><label class="container"><input type="checkbox" name="Momos" value="Momos 60"/><span class="checkmark"></span></label></center></td>
+	</tr>
+</table>
+  
+  
+<table class="t" border="0px" >
+	<tr margin="2px" ></tr>
+	<tr>
+		<td rowspan="2" width="20%" ><img src="chive dumpling.jpeg" alt="Chive Dumpling" width="125px" height="120px" ></td>
+		<td width="80%"><b style="font-family : Arial, Helvetica, sans-serif;">Chive Dumplings</b></td>
+		<td><center><img src="http://localhost/WTProject/Home/VegSymb.png" width="25px" height="25px"></center></td>
+	</tr>
+	<tr>
+		<td><pre><h3><b>Rs 65 </b></h3></pre></td>
+		<td rowspan="2"><center><label class="container"><input type="checkbox" name="Chive_Dumpling" value="Chive_Dumpling 65"/><span class="checkmark"></span></label></center></td>
+		<form method="POST" action="WTChinese.php">
+<input type="checkbox">
+
+</tr>
+</table>
+<br />
+	<center><input class="sub" type="submit" value="Add to Cart" /></center>
+
+ 		
+		
+			<p style="color : brown; text-align : center; font-size : 13px; font-family : Arial, Helvetica, sans-serif;">contact Us:9658745821</p>
+		</div>
+	</footer>
+</body>
+</html>
+
+
